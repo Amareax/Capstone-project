8cbd77973005bb95a4777fb2bcd08039631b7650
Amareax <amareprudencia@gmail.com>
2023-08-26 17:18:00 +0200

    Initial Commit

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by YOLISA MAGIBILE on 2023/08/26.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
